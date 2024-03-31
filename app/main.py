06831b17d292dafafa526218fa70349fc0be5d35
NioviIki <charmniovi@tutanota.com>
2024-03-31 21:41:41 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -11,30 +11,16 @@ def copy_file(command: str):
 """
 
 
-def get_files_with_double_quotes(command):
-    return [i for i in command.split("\"") if i and i != " "]
-
-
-def get_files_with_shielding(command):
-    source_destination = command.replace("\ ", " ")  # NOQA
-    source_parts = source_destination.split(".")
-
-    source_file = source_parts[0] + "." + source_parts[1].split()[0]
-    destination_file = source_destination[len(source_file) + 1:]
-    return source_file, destination_file
-
-
 def copy_file(command: str) -> None:
     if command[:3] == "cp":
         raise ValueError(
             "Expected 'cp' as the first argument"
         )
     command = command[3:]
+    source_parts = command.split(".")
 
-    if "\"" in command:
-        source_file, destination_file = get_files_with_double_quotes(command)
-    else:
-        source_file, destination_file = get_files_with_shielding(command)
+    source_file = source_parts[0] + "." + source_parts[1].split()[0]
+    destination_file = command[len(source_file) + 1:]
 
     if not destination_file or len(destination_file.split(".")) > 2:
         raise ValueError(
